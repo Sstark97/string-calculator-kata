d6e6eb5e2149e2df9cb45ff27dff629b11a7e8be
Sstark97 <aitorscinfo@gmail.com>
2023-03-21 09:58:00 +0000

    Test(green): should_add_two_numbers_separated_by_commas

--- src/core/stringCalculator.ts
+++ src/core/stringCalculator.ts
@@ -1,5 +1,8 @@
 export const add = (theOperation: string) => {
     const emptyOperation = "0";
     const theOperationIsNotEmpty = theOperation !== "";
+    if(theOperation[0] && theOperation[2]) {
+        return (parseInt(theOperation[0]) + parseInt(theOperation[2])).toString()
+    }
     return theOperationIsNotEmpty ? theOperation : emptyOperation
 }
