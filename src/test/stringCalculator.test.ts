adbff258ffec5bc97c4ee7c9a2156bb31fcd31fa
Sstark97 <aitorscinfo@gmail.com>
2023-03-21 09:44:37 +0000

    Test(red): should_be _'0'_if_received_an_empty_parameter

--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -1,5 +1,7 @@
 import {add} from "../core/stringCalculator";
 
-describe("String Calculator test", () => {
-
+describe("String Calculator", () => {
+    it("should be '0' if received an empty parameter", () => {
+        expect(add("")).toBe("0")
+    })
 })
