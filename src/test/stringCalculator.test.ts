2c9f8392c051f73f353aa892b0d6fa640259b2fb
Sstark97 <aitorscinfo@gmail.com>
2023-03-21 09:48:54 +0000

    Test(red): should_be_the_same_number_if_the_parameter_contains_only_one_number

--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -4,4 +4,8 @@ describe("String Calculator", () => {
     it("should be '0' if received an empty parameter", () => {
         expect(add("")).toBe("0")
     })
+
+    it("should be the same number if the parameter contains only one number", () => {
+        expect(add("1")).toBe("1")
+    })
 })
